fa6ba7988a68c479e15d560a0f4aff51967ce722
suresh a <asureshdevops10@gmail.com>
2021-07-19 21:00:55 +0530

    new

---
+++ src/main/java/banking/com/tata/TataApplication.java
@@ -0,0 +1,13 @@
+package banking.com.tata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TataApplication.class, args);
+	}
+
+}
